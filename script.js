0278a1c2b0c3ad4536408ce2679771b70a371ff9
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-03-21 20:37:53 -0700

    enable  users to add new books to current library

--- script.js
+++ script.js
@@ -11,23 +11,33 @@ function addBookToLibrary(book) {
 
 let frankenstein = new Book("Frankenstein");
 addBookToLibrary(frankenstein);
-addBookToLibrary(frankenstein);
-addBookToLibrary(frankenstein);
-addBookToLibrary(frankenstein);
-addBookToLibrary(frankenstein);
+let leanStartup = new Book("The Lean Startup");
+addBookToLibrary(leanStartup);
+let tedTalks = new Book("TED Talks");
+addBookToLibrary(tedTalks);
 
 const currentLibraryDiv = document.getElementById("currentLibrary");
-
-for (let book of myLibrary) {
-  const newBookCardDiv = document.createElement("div");
-  newBookCardDiv.className = "newBookCard";
-  newBookCardDiv.textContent = book.name;
-  currentLibraryDiv.appendChild(newBookCardDiv);
-}
 
 const addBookBtn = document.getElementById("addBookBtn");
 const bookNameInput = document.getElementById("bookName");
 
 addBookBtn.addEventListener("click", (event) => {
   event.preventDefault();
+  const newBook = new Book(bookNameInput.value);
+  myLibrary.push(newBook);
+  renderCurrentBooks();
+  bookNameInput.value = "";
+  bookNameInput.focus();
 });
+
+function renderCurrentBooks() {
+  currentLibraryDiv.innerHTML = null;
+  for (let book of myLibrary) {
+    const newBookCardDiv = document.createElement("div");
+    newBookCardDiv.className = "newBookCard";
+    newBookCardDiv.textContent = book.name;
+    currentLibraryDiv.appendChild(newBookCardDiv);
+  }
+}
+
+renderCurrentBooks();
