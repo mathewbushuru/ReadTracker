e4c17dcab35d04e0cc247c8c5d09cf65c9b7359e
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-03-21 20:12:21 -0700

    add book display

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 let myLibrary = [];
 
-function Book(){
-    // Book constructor
+function Book(name) {
+  // Book constructor
+  this.name = name;
 }
 
-function addBookToLibrary(){
-    // do stuff here
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+let frankenstein = new Book("Frankenstein");
+addBookToLibrary(frankenstein);
+addBookToLibrary(frankenstein);
+addBookToLibrary(frankenstein);
+addBookToLibrary(frankenstein);
+addBookToLibrary(frankenstein);
+
+const currentLibraryDiv = document.getElementById("currentLibrary");
+
+for (let book of myLibrary) {
+  const newBookCardDiv = document.createElement("div");
+  newBookCardDiv.className = "newBookCard";
+  newBookCardDiv.textContent = book.name;
+  currentLibraryDiv.appendChild(newBookCardDiv);
 }
